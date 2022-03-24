d5976a6b1f800321e78e0fe1cb3419486d0d3dcd
Alfalmi <pinooxd@gmail.com>
2022-03-24 13:50:09 -0700

    as

---
+++ src/theme/Theme.ts
@@ -0,0 +1,31 @@
+const Theme = {
+  colors: {
+    light: {
+      primary: '#F5FCEA',
+      secondary: '#ddd',
+    },
+    dark: {
+      primary: '#3B3D39',
+      secondary: '#444',
+    },
+    red: '#F40808',
+    green: '#38FF00',
+  },
+  padding: {
+    sm: '1rem',
+    md: '2rem',
+    lg: '4rem',
+  },
+  width: {
+    sm: '500px',
+    md: '1000px',
+    lg: '1500px',
+  },
+  borderRadius: {
+    sm: '5px',
+    md: '10px',
+    lg: '20px',
+  },
+}
+
+export default Theme
